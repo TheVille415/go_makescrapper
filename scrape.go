a31c3beb4eb2a807fee8cc5b48ea8c7ddab10f95
Jordan <jt.arontorres@gmail.com>
2022-03-03 20:53:18 -0800

    working on lucifer scraper

--- scrape.go
+++ scrape.go
@@ -1,30 +1,53 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 
 	"github.com/gocolly/colly"
 )
+
+type Lucy struct {
+	Episode string
+	Title   string
+	Airdate string
+}
+
+func createJson(characters []Lucy) {
+	jsonFile, _ := json.MarshalIndent(characters, "", " ")
+	_ = ioutil.WriteFile("Lucy.json", jsonFile, 0644)
+}
 
 // main() contains code adapted from example found in Colly's docs:
 // http://go-colly.org/docs/examples/basic/
 func main() {
 	// Instantiate default collector
 	c := colly.NewCollector()
+	character := make([]Lucy, 0)
 
 	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-                link := e.Attr("href")
-
-		// Print link
-                fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+	c.OnHTML("table.general", func(e *colly.HTMLElement) {
+		e.ForEach("tr.general-header", func(_ int, e *colly.HTMLElement) {
+			newInfo := Lucy{}
+			newInfo.Episode = e.ChildText("td")
+			newInfo.Title = e.ChildText("a")
+			newInfo.Airdate = e.ChildText("td")
+			character = append(character, newInfo)
+		})
 	})
-
 	// Before making a request print "Visiting ..."
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL.String())
 	})
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Println("Got a response from", r.Request.URL)
+	})
 
-	// Start scraping on https://hackerspaces.org
-	c.Visit("https://hackerspaces.org/")
+	c.OnError(func(r *colly.Response, e error) {
+		fmt.Println("Received error:", e)
+	})
+
+	// Start scraping on https://lucifer.fandom.com/wiki/Episodes
+	c.Visit("https://lucifer.fandom.com/wiki/Episodes")
 }
